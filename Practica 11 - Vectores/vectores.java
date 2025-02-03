95f7da86d3216489797e4d70f9647768d67100b9
SebasInfoBSudamericano <166545739+SebasInfoBSudamericano@users.noreply.github.com>
2025-02-03 13:41:36 -0500

    Added Content of Practica 11

---
+++ Practica 11 - Vectores/vectores.java
@@ -0,0 +1,19 @@
+public class vectores {
+    public static void main(String[] args) {
+        // Declaración de un vector
+        int[] num = new int[5];
+        // Alimentar a un vector
+        // Asignamos la posición y el valor
+        num[0] = 10;
+        num[1] = 20;
+        num[2] = 30;
+        num[3] = 40;
+        num[4] = 50;
+        // Presentar los valores en pantalla
+        System.out.print("["+num[0]+"]");
+        System.out.print("["+num[1]+"]");
+        System.out.print("["+num[2]+"]");
+        System.out.print("["+num[3]+"]");
+        System.out.print("["+num[4]+"]");
+    }
+}
